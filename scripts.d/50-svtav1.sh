fe8f157091cc96fa499f38fc7c1cb329ea09b260
Exalt the Dx <52926952+dnshtm9@users.noreply.github.com>
2024-09-22 01:08:35 +0700

    Update 50-svtav1.sh

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
-SCRIPT_COMMIT="85e44db4b49fcbf8fea2472bdd517e8aa8e1c1a0"
+SCRIPT_REPO="https://github.com/gianni-rosato/svt-av1-psy.git"
+SCRIPT_COMMIT="7077c9ff6f638328f6a5139fafcc2d8d537ebb77"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
